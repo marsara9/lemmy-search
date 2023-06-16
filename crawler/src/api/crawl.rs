ea05d6b945ce32bbe1c4142fbec4257e08057897
Parker Day <marsara9@gmail.com>
2023-06-16 19:57:47 -0400

    Starting work on crawler.

---
+++ crawler/src/api/crawl.rs
@@ -0,0 +1,63 @@
+use serde::{Serialize, de::DeserializeOwned};
+use lemmy_api_common::{
+    community::{
+        ListCommunities, 
+        ListCommunitiesResponse
+    }, 
+    lemmy_db_schema::{
+        ListingType, 
+        SortType, newtypes::CommunityId
+    }, post::{GetPosts, GetPostsResponse}
+};
+use reqwest::Client;
+
+const DEFAULT_LIMIT : i64 = 50;
+
+async fn fetch_json<T: Serialize + ?Sized, R: DeserializeOwned>(
+    url : String,
+    params : &T
+) -> R {
+    let client = Client::new();
+    return client
+        .get(url)
+        .query(&params)
+        .send()
+        .await
+        .unwrap()
+        .json()
+        .await
+        .unwrap();
+}
+
+pub async fn fetch_posts(
+    instance : &str,
+    community_id : CommunityId,
+    page: i64
+) -> GetPostsResponse {
+    let params = GetPosts {
+        community_id: Some(community_id),
+        page: Some(page),
+        sort: Some(SortType::Old),
+        limit: Some(DEFAULT_LIMIT),
+        ..Default::default()
+    };
+
+    let url = format!("https://{}/api/v3/community/list", instance);    
+    return fetch_json::<GetPosts, GetPostsResponse>(url, &params)
+        .await;
+}
+
+pub async fn fetch_all_communities(
+    instance : &str
+) -> ListCommunitiesResponse {
+    let params = ListCommunities {
+        type_: Some(ListingType::Local),
+        sort: Some(SortType::Old),
+        limit: Some(DEFAULT_LIMIT),
+        ..Default::default()
+    };
+
+    let url = format!("https://{}/api/v3/community/list", instance);    
+    return fetch_json::<ListCommunities, ListCommunitiesResponse>(url, &params)
+        .await;
+}
--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    
+use reqwest::Result;
+mod api;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+    let communities = api::crawl::fetch_all_communities(
+        "voyager.lemmy.ml"
+    ).await;
+
+    print!("{:?}", &communities);
+
+    Ok(())
 }
