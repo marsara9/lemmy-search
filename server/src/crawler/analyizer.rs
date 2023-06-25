1e19452a6538ac775dde1e529ff5f98c7d3c54d9
Parker Day <marsara9@gmail.com>
2023-06-24 22:22:13 -0400

    Further refining analyizer

--- server/src/crawler/analyizer.rs
+++ server/src/crawler/analyizer.rs
@@ -20,17 +20,17 @@ impl Analyizer {
         let mut words = HashSet::<String>::new();
         let name_trimed = post.name.replace(|c : char| {
             !c.is_ascii_alphanumeric() && !c.is_whitespace()
-        }, "").to_lowercase();
+        }, " ").to_lowercase();
         for word in name_trimed.split_whitespace() {
-            words.insert(word.to_lowercase().to_string());
+            words.insert(word.to_lowercase().trim().to_string());
         }
         match &post.body {
             Some(body) => {
                 let body_trimed = body.replace(|c : char| {
                     !c.is_ascii_alphanumeric() && !c.is_whitespace()
-                }, "").to_lowercase();
+                }, " ").to_lowercase();
                 for word in body_trimed.split_whitespace() {
-                    words.insert(word.to_lowercase().to_string());
+                    words.insert(word.to_lowercase().trim().to_string());
                 }
             },
             None => {}
@@ -44,8 +44,8 @@ impl Analyizer {
     ) -> HashSet<String> {
         HashSet::from_iter(comment.content.replace(|c : char| {
             !c.is_ascii_alphanumeric() && !c.is_whitespace()
-        }, "").to_lowercase().split_whitespace().map(|word|
-            word.to_lowercase().to_string()
+        }, " ").to_lowercase().split_whitespace().map(|word|
+            word.to_lowercase().trim().to_string()
         ))
     }
 }
