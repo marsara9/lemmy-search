c629d080ac2ef2549400dafdede0baaf20b672fc
Parker Day <marsara9@gmail.com>
2023-07-17 15:34:49 -0400

    Removing unnecessary JavaScript

--- ui/shared/rest.js
+++ ui/shared/rest.js
@@ -1,18 +1,8 @@
-function restJson(url, method, body, showSpinner, onResult, onError) {
+function restJson(url, method, onResult, onError) {
 
     let params = {
         method: method,
-        credentials: "same-origin",
-        cache: "no-cache",
         headers: {}
-    }
-    if(body) {
-        params.body = JSON.stringify(body)
-        params.headers["content-type"] = "application/json"
-    }
-
-    if(showSpinner) {
-        $("#loading-dialog")[0].showModal()
     }
 
     fetch(url, params).then(response => {        
@@ -38,28 +28,9 @@ function restJson(url, method, body, showSpinner, onResult, onError) {
                 onError(response.body)
             }
         }
-        if(response.status == 401) {
-            logout()          
-        }
-    }).then(() => {
-        if(showSpinner) {
-            $("#loading-dialog")[0].close()
-        }
     })
 }
 
 function fetchJson(url, onResult, onError) {
     restJson(url, "GET", null, false, onResult, onError)
-}
-
-function postJson(url, data, onResult, onError) {
-    restJson(url, "POST", data, true, onResult, onError)
-}
-
-function putJson(url, data, onResult, onError) {
-    restJson(url, "PUT", data, true, onResult, onError)
-}
-
-function deleteJson(url, data, onResult, onError) {
-    restJson(url, "DELETE", data, true, onResult, onError)
 }
